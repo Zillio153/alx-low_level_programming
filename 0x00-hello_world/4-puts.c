0d9ca48b24869177e0e4a0d57201456fd2d58788
Emmanuel Uche Onyenanu <uchee7154@gmail.com>
2023-02-20 03:27:50 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry write a c program that prints a line with put function
+ * main - Entry write a C program that prints a line with put function
  * Return: Always 0
  */
 int main(void)
