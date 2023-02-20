507e6134e0ecdbf94db796407363fdba2bb29191
Emmanuel Uche Onyenanu <uchee7154@gmail.com>
2023-02-20 05:52:57 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
